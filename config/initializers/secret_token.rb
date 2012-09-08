119d202da89a27eabf1df00cea4b6961ea81d6dd
Matthew Dale <matthew@server.ctf.local>
2012-09-08 15:22:03 -0400

    Initialize Rails 3.2.8 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrowserlessAuth::Application.config.secret_token = '04a7f4feeb0734a38055184ce702122b0fa96ee7fe348f7cc17ebe9110650f220a45d99c2f67ad5089dd9b87c6854a6ffd7cb5bc080f510440ee0327bcd5faa2'
